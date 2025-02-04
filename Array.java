8522702c490260c5cd563211fdf79e582a934855
ErikaFurudo111297 <148293084+ErikaFurudo111297@users.noreply.github.com>
2025-02-04 11:48:23 +0000

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,11 @@
+public class Array {
+    public static void main(String[] args) {
+        int[] Array = {1, 2, 3, 4, 5};
+
+        System.out.printf("%d", Array[4]);
+        System.out.printf("%d", Array[1]);
+        System.out.printf("%d", Array[0]);
+        System.out.printf("%d", Array[3]);
+        System.out.printf("%d", Array[2]);
+    }
+}
